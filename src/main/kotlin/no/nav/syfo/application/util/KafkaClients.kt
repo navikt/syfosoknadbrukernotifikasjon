aa3a8818d4b07c32da15284219e1eab7c71cc239
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2020-11-05 15:21:43 +0100

    none

--- src/main/kotlin/no/nav/syfo/application/util/KafkaClients.kt
+++ src/main/kotlin/no/nav/syfo/application/util/KafkaClients.kt
@@ -17,7 +17,7 @@ class KafkaClients(env: Environment) {
     private fun syfoSoknadConsumer(env: Environment): KafkaConsumer<String, String> {
 
         val config = loadBaseConfig(env, env.hentKafkaCredentials()).envOverrides()
-        config["auto.offset.reset"] = "latest"
+        config["auto.offset.reset"] = "none"
 
         val properties = config.toConsumerConfig("${env.applicationName}-consumer", StringDeserializer::class)
         properties.let { it[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = "1" }
