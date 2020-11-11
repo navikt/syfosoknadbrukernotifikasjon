0ba2e599285c00abd44bf1a8358387d103db5c19
kulsveen <bjorn.inge.kulsveen@nav.no>
2020-11-11 09:51:57 +0100

    Dry run - mistede kafka meldinger

--- src/main/kotlin/no/nav/syfo/soknad/service/SpoleService.kt
+++ src/main/kotlin/no/nav/syfo/soknad/service/SpoleService.kt
@@ -11,8 +11,8 @@ import no.nav.syfo.kafka.loadBaseConfig
 import no.nav.syfo.kafka.toConsumerConfig
 import no.nav.syfo.log
 import no.nav.syfo.soknad.db.finnBrukernotifikasjon
+import no.nav.syfo.soknad.domene.EnkelSykepengesoknad
 import no.nav.syfo.soknad.domene.tilEnkelSykepengesoknad
-import no.nav.syfo.soknad.service.SykepengesoknadBrukernotifikasjonService.EnkelSykepengesoknadUtil.kanFåDonemeldingUtenAtBrukernotfifikasjonErIDatabasen
 import no.nav.syfo.soknad.service.SykepengesoknadBrukernotifikasjonService.EnkelSykepengesoknadUtil.skalOppretteOppgave
 import no.nav.syfo.soknad.service.SykepengesoknadBrukernotifikasjonService.EnkelSykepengesoknadUtil.skalSendeDoneMelding
 import org.apache.kafka.clients.consumer.ConsumerConfig
@@ -106,23 +106,12 @@ class SpoleService(
                 }.onFailure { e ->
                     log.info("SpoleService kunne ikke deserialisere søknad, fortsetter", e)
                 }.onSuccess { sykepengesoknad ->
-                    if (sykepengesoknad.skalOppretteOppgave()) {
-                        val brukernotfikasjon = database.finnBrukernotifikasjon(sykepengesoknad.id)
-                        if (brukernotfikasjon == null) {
-                            if (sykepengesoknad.kanFåDonemeldingUtenAtBrukernotfifikasjonErIDatabasen() == false) {
-                                log.info("SpoleService finner ikke: dittnav oppgave for søknad ${sykepengesoknad.id}")
-                            }
-                        }
-                    } else if (sykepengesoknad.skalSendeDoneMelding()) {
-                        val brukernotfikasjon = database.finnBrukernotifikasjon(sykepengesoknad.id)
-                        if (brukernotfikasjon != null) {
-                            if (brukernotfikasjon.doneSendt == null) {
-                                log.info("SpoleService finner ikke: done melding for søknad ${sykepengesoknad.id}")
-                            }
-                        }
+                    val kandidat = rebehandlinger.find { it.soknadId == sykepengesoknad.id }
+                    if (kandidat != null) {
+                        kandidat.sisteSoknad = sykepengesoknad
+                        consumer.commitSync()
                     }
                 }
-                consumer.commitSync()
             }
 
             poll = consumer.poll(Duration.ofMillis(1000))
@@ -131,5 +120,52 @@ class SpoleService(
                 consumer.commitSync()
             }
         }
+
+        // DRY RUN
+        rebehandlinger.forEach {
+            if (it.sisteSoknad == null) {
+                log.info("DRY RUN - SpoleService finner ikke søknad ${it.soknadId}, skal ikke skje!")
+            } else if (it.sisteSoknad!!.skalOppretteOppgave() && database.finnBrukernotifikasjon(it.soknadId) == null) {
+                log.info("DRY RUN - SpoleService ville opprettet oppgave for ${it.soknadId}")
+            } else if (it.sisteSoknad!!.skalSendeDoneMelding() && database.finnBrukernotifikasjon(it.soknadId) != null && database.finnBrukernotifikasjon(it.soknadId)?.doneSendt == null) {
+                log.info("DRY RUN - SpoleService ville sendt done melding for ${it.soknadId}")
+            } else {
+                log.info("DRY RUN - SpoleService gikk til else for ${it.soknadId}, skal ikke skje!")
+            }
+        }
     }
+
+    companion object Rebehandlinger {
+        val rebehandlinger = listOf(
+            Rebehandling(soknadId = "e8dea914-473a-4b09-9c18-a115e61ea80d"),
+            Rebehandling(soknadId = "118bc72c-11af-4a82-acaa-8e7ac0e21b58"),
+            Rebehandling(soknadId = "0839a1b3-9385-4346-a3cb-8327cbd16490"),
+            Rebehandling(soknadId = "63c03823-1025-4065-a6bc-664e294eaf41"),
+            Rebehandling(soknadId = "c5e825b7-aee3-448c-8e63-ee2826fecffd"),
+            Rebehandling(soknadId = "9e33fe59-1904-4298-86cb-7bc7e59daf4c"),
+            Rebehandling(soknadId = "63abb776-60d4-454e-add1-f95cf12c6264"),
+            Rebehandling(soknadId = "525f6ed9-409f-4914-857f-7052598ceb76"),
+            Rebehandling(soknadId = "94c79950-46a7-4958-a925-53e2b037b021"),
+            Rebehandling(soknadId = "94c79950-46a7-4958-a925-53e2b037b021"),
+            Rebehandling(soknadId = "ba2b1120-a76c-441d-937d-bc68a7ef9f19"),
+            Rebehandling(soknadId = "20466234-8c4a-4ca9-9a1e-6aae7f7ab74f"),
+            Rebehandling(soknadId = "59c64c09-c077-4104-8aa4-8219cebeef01"),
+            Rebehandling(soknadId = "76a2cab5-678f-40f1-9d86-7e188d130b63"),
+            Rebehandling(soknadId = "384d5931-8b35-431f-a1cc-01f94011443c"),
+            Rebehandling(soknadId = "75ce402e-cab3-4f69-b30b-9c192fdb88b9"),
+            Rebehandling(soknadId = "e3699477-f4e3-4626-8948-f9082efd0fb1"),
+            Rebehandling(soknadId = "7318dfd4-6394-4cd3-8271-06fa307cfdd3"),
+            Rebehandling(soknadId = "89f07c44-bc7b-4440-b550-5f8b73c38dbd"),
+            Rebehandling(soknadId = "4bab0103-57f1-4dcf-b751-11374ee49afd"),
+            Rebehandling(soknadId = "fa109adc-2e9d-4258-a292-6c1c2524e814"),
+            Rebehandling(soknadId = "3041ecb4-4da6-4bb8-94ad-f0be4214d360"),
+            Rebehandling(soknadId = "2a9c50ce-db3d-40b6-841d-25cb1ad31d68"),
+            Rebehandling(soknadId = "2a664894-bb53-42c1-865a-61cbe71329df")
+        )
+    }
+
+    data class Rebehandling(
+        val soknadId: String,
+        var sisteSoknad: EnkelSykepengesoknad? = null
+    )
 }
