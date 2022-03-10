f427d0c7d5d0206dcc4b72a85aae3c88f337f182
kulsveen <bjorn.inge.kulsveen@nav.no>
2022-03-10 13:03:15 +0100

    Legger til KafkaAvroSerializerConfig

--- src/main/kotlin/no/nav/helse/flex/kafka/ProducerKafkaConfig.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/ProducerKafkaConfig.kt
@@ -2,11 +2,13 @@ package no.nav.helse.flex.kafka
 
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig
 import io.confluent.kafka.serializers.KafkaAvroSerializer
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig
 import no.nav.brukernotifikasjon.schemas.input.DoneInput
 import no.nav.brukernotifikasjon.schemas.input.NokkelInput
 import no.nav.brukernotifikasjon.schemas.input.OppgaveInput
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.config.SaslConfigs
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -17,6 +19,8 @@ import org.springframework.context.annotation.Profile
 class ProducerKafkaConfig(
     private val aivenKafkaConfig: AivenKafkaConfig,
     @Value("\${KAFKA_SCHEMA_REGISTRY}") private val kafkaSchemaRegistryUrl: String,
+    @Value("\${KAFKA_SCHEMA_REGISTRY_USER}") private val kafkaSchemaUsername: String,
+    @Value("\${KAFKA_SCHEMA_REGISTRY_PASSWORD}") private val kafkaSchemaPassword: String,
 ) {
 
     private fun producerConfig(): Map<String, Any> {
@@ -28,7 +32,10 @@ class ProducerKafkaConfig(
             ProducerConfig.RETRIES_CONFIG to "100000",
             ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to KafkaAvroSerializer::class.java,
             ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to KafkaAvroSerializer::class.java,
-            AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to kafkaSchemaRegistryUrl
+            AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to kafkaSchemaRegistryUrl,
+            KafkaAvroSerializerConfig.USER_INFO_CONFIG to "$kafkaSchemaUsername:$kafkaSchemaPassword",
+            KafkaAvroSerializerConfig.BASIC_AUTH_CREDENTIALS_SOURCE to "USER_INFO",
+            SaslConfigs.SASL_MECHANISM to "PLAIN",
         ) + aivenKafkaConfig.commonConfig()
     }
 
