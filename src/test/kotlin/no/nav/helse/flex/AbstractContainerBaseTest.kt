3daa90f105067c066ad0d9a5b76641db2ed2a95d
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-09-20 11:53:10 +0200

    Bump Kafka Testcontainers-versjon til 7.4.1

--- src/test/kotlin/no/nav/helse/flex/AbstractContainerBaseTest.kt
+++ src/test/kotlin/no/nav/helse/flex/AbstractContainerBaseTest.kt
@@ -31,7 +31,7 @@ abstract class AbstractContainerBaseTest {
                 System.setProperty("spring.datasource.password", it.password)
             }
 
-            KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.0.1")).also {
+            KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.1")).also {
                 it.start()
                 System.setProperty("KAFKA_BROKERS", it.bootstrapServers)
             }
