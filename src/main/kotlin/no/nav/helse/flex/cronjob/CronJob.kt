5d2a01c6d92ff96e45f1b7cd0abd84bbe4ea9d4f
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-03-21 11:41:32 +0100

    Revert "Skru av brukernotifkasjonprodusering mens vi fikser fta (#348)"
    This reverts commit 87ce033b21cd008bec37d4d4693a90662b7e99e8.

--- src/main/kotlin/no/nav/helse/flex/cronjob/CronJob.kt
+++ src/main/kotlin/no/nav/helse/flex/cronjob/CronJob.kt
@@ -2,7 +2,9 @@ package no.nav.helse.flex.cronjob
 
 import no.nav.helse.flex.brukernotifikasjon.BrukernotifikasjonOpprettelse
 import no.nav.helse.flex.logger
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
 
 @Component
 class CronJob(
@@ -11,6 +13,11 @@ class CronJob(
 ) {
     val log = logger()
 
+    @Scheduled(
+        initialDelay = 2,
+        fixedDelay = 10,
+        timeUnit = TimeUnit.MINUTES,
+    )
     fun run() {
         if (leaderElection.isLeader()) {
             log.info("Kjører brukernotifikasjonjob")
