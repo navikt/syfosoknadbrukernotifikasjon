e43a12b2e955a654f9f600d8d51dade1ae3b7e3d
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-03-21 08:46:09 +0100

    Skru av brukernotifkasjonprodusering mens vi fikser fta

--- src/main/kotlin/no/nav/helse/flex/cronjob/CronJob.kt
+++ src/main/kotlin/no/nav/helse/flex/cronjob/CronJob.kt
@@ -2,9 +2,7 @@ package no.nav.helse.flex.cronjob
 
 import no.nav.helse.flex.brukernotifikasjon.BrukernotifikasjonOpprettelse
 import no.nav.helse.flex.logger
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import java.util.concurrent.TimeUnit
 
 @Component
 class CronJob(
@@ -13,11 +11,6 @@ class CronJob(
 ) {
     val log = logger()
 
-    @Scheduled(
-        initialDelay = 2,
-        fixedDelay = 10,
-        timeUnit = TimeUnit.MINUTES,
-    )
     fun run() {
         if (leaderElection.isLeader()) {
             log.info("Kjører brukernotifikasjonjob")
