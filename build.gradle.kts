a455b1a00619b70d02f90369179f0e5d42997378
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2025-12-09 12:59:55 +0100

    Bump Testcontainers til 2.0.2 (#390)

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ repositories {
     }
 }
 
-val testContainersVersion = "1.21.3"
+val testContainersVersion = "2.0.2"
 val logstashLogbackEncoderVersion = "8.1"
 val kluentVersion = "1.73"
 val varselKotlinBuilderVersion = "1.1.0"
@@ -50,8 +50,11 @@ dependencies {
         exclude(group = "com.fasterxml.jackson.datatype", module = "jackson-datatype-jsr310")
         exclude(group = "com.fasterxml.jackson.module", module = "jackson-module-kotlin")
     }
-    testImplementation(platform("org.testcontainers:testcontainers-bom:$testContainersVersion"))
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.testcontainers:testcontainers:$testContainersVersion")
+    testImplementation("org.testcontainers:testcontainers-postgresql:$testContainersVersion")
+    testImplementation("org.testcontainers:testcontainers-kafka:$testContainersVersion")
+
     testImplementation("org.testcontainers:postgresql")
     testImplementation("org.testcontainers:kafka")
     testImplementation("org.awaitility:awaitility")
